7b75fe11e645fd5af669e217636759438f63bee2
Radalyak <Radishalexei@gmail.com>
2021-08-15 22:04:29 +0300

    3. Тесты для определения расстояния между точками. [#159459] fixed whitespace

--- test/ru/job4j/codition/PointTest.java
+++ test/ru/job4j/codition/PointTest.java
@@ -42,7 +42,7 @@ public class PointTest {
         int y1 = 0;
         int x2 = 7;
         int y2 = 5;
-        double out = Point.distance(x1,y1, x2, y2);
+        double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
