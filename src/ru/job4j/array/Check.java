2c9b702ea456775bc70054134e2dddc6c90fed10
Radalyak <Radishalexei@gmail.com>
2021-08-31 15:07:53 +0300

    6.3. Массив заполнен true или false [#53857] fixed break&i=1

--- src/ru/job4j/array/Check.java
+++ src/ru/job4j/array/Check.java
@@ -3,10 +3,10 @@ package ru.job4j.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
-        for (int i = 0; i < data.length; i++) {
+        for (int i = 1; i < data.length; i++) {
             if (data[0] != data[i]) {
-                result = false; // я бы сюда добавил break чтобы не проверять
-                                // массив дальше если он уже не однородный
+                result = false;
+                break;
             }
         }
         return result;
