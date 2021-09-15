68edd093876d35d998716482c06ecf52767622df
Radalyak <Radishalexei@gmail.com>
2021-09-15 11:44:23 +0300

    hard4me exercise simplified

--- src/main/java/ru/job4j/loop/Cryptography.java
+++ src/main/java/ru/job4j/loop/Cryptography.java
@@ -2,22 +2,14 @@ package ru.job4j.loop;
 
 public class Cryptography {
     public static String code(String s) {
-        if (s == "") {
+
+        if (s.isEmpty()) {
             return "empty";
-        } else {
-            int len = s.length();
-            StringBuilder str = new StringBuilder(s);
-            System.out.println(str);
-            for (int i = len - 1; i >= 0; i--) {
-                char temp = s.charAt(i);
-                if (i < len - 4) {
-                    str.setCharAt(i, '#');
-                } else {
-                    str.setCharAt(i, temp);
-                }
-            }
-            String out = new String(str);
-            return out;
         }
-    }
-}
+            StringBuilder str = new StringBuilder(s);
+            for (int i = 0; i < str.length() - 4; i++) {
+                    str.setCharAt(i, '#');
+                }
+        return str.toString();
+            }
+        }
